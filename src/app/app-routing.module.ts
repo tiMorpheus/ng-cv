418aa70af9c020377915865217894925855ff0b0
timorpheus <tymurtolochko@gmail.com>
2019-02-05 16:48:33 +0200

    builded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import {AppComponent} from "./app.component";
 
 const routes: Routes = [
-    {path: '', component: AppComponent}
+    {path: 'ng-cv', component: AppComponent}
 ];
 
 @NgModule({
